80cf7034347b354293acade07bed13fe4b59d249
Sameed Imad <sameedimad188@email.com>
2026-06-14 21:21:48 +0530

    update

--- src/Components/Mywork/MyWork.jsx
+++ src/Components/Mywork/MyWork.jsx
@@ -3,25 +3,40 @@ import './MyWork.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import mydata from '../../assets/mywork_data'
 const MyWork = () => {
-  return (
-    <div id="work" className="mywork">
-        <div className="mywork-title">
-            <h1>My latest work</h1>
-            <img className="theme" src={theme_pattern} alt="" />
-        </div>
-        <div className="mywork-container">
-            {mydata.map((work, index) => (
-                <div className="work-card" key={index}>
-                    <img src={work.w_img} alt="project" />
-                    <div className="overlay">
-                        <a href="https://project-web-application-baqo.onrender.com/listings" target="_blank" rel="noreferrer" className="btn live">Live</a>
-                        <a href="https://github.com/SameedImad/WanderLust-Full-Stack-Travel-Listing-Web-Application" target="_blank" rel="noreferrer" className="btn demo">Code</a>
+    return (
+        <div id="work" className="mywork">
+            <div className="mywork-title">
+                <h1>My latest work</h1>
+                <img className="theme" src={theme_pattern} alt="" />
+            </div>
+            <div className="mywork-container">
+                {mydata.map((work, index) => (
+                    <div className="work-card" key={index}>
+                        <img src={work.w_img} alt="project" />
+                        <div className="overlay">
+                            <a
+                                href={work.live_link}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="btn live"
+                            >
+                                Live
+                            </a>
+
+                            <a
+                                href={work.github_link}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="btn demo"
+                            >
+                                Code
+                            </a>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default MyWork
